92fa4ed6706d1fd4120112eaa0ace8cad4158484
Henry G. Bravo <hgbravo@icloud.com>
2017-11-30 02:15:15 -0500

    First commit and create model Dish class

---
+++ app/src/main/java/com/hbravodev/rest2serve/activity/MenuActivity.kt
@@ -0,0 +1,13 @@
+package com.hbravodev.rest2serve.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hbravodev.rest2serve.R
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
---
+++ app/src/main/java/com/hbravodev/rest2serve/model/Dish.kt
@@ -0,0 +1,4 @@
+package com.hbravodev.rest2serve.model
+
+class Dish(val name: String, val allergens: Array<String>?, val price: Float, val description: String) {
+}
