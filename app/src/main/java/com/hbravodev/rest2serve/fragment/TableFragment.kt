ee0072bfc416497eac1287fc6839b1c9d305cdc5
Henry G. Bravo <hgbravo@icloud.com>
2018-01-26 18:37:38 -0500

    Implement custom list row for ordered dishes

--- app/src/main/java/com/hbravodev/rest2serve/fragment/TableFragment.kt
+++ app/src/main/java/com/hbravodev/rest2serve/fragment/TableFragment.kt
@@ -7,11 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import android.widget.ListView
+import android.widget.TextView
 import com.hbravodev.rest2serve.R
 import com.hbravodev.rest2serve.model.Dish
+import com.hbravodev.rest2serve.model.DishOrdered
 import com.hbravodev.rest2serve.model.Tables
+import kotlinx.android.synthetic.main.dish_ordered_list_row.view.*
 import java.util.ArrayList
+import java.util.zip.Inflater
 
 class TableFragment : Fragment() {
 
@@ -29,8 +34,9 @@ class TableFragment : Fragment() {
 
     private lateinit var root: View
     private var tableIndex  = 0
+    private var orderedDishes = ArrayList<DishOrdered>()
     var list: ListView? = null
-    private lateinit var adapter: ArrayAdapter<Dish>
+    private lateinit var adapter: MyListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,9 +49,10 @@ class TableFragment : Fragment() {
         // Inflate the layout for this fragment
         if (inflater != null) {
             tableIndex = arguments.getInt(ARG_TABLE_INDEX)
+            orderedDishes = Tables[tableIndex].items as ArrayList<DishOrdered>
             root = inflater.inflate(R.layout.fragment_table, container, false)
             list = root.findViewById(R.id.dishes_list)
-            adapter = ArrayAdapter(activity, android.R.layout.simple_list_item_1, Tables[tableIndex].dishes())
+            adapter = MyListAdapter()
             list?.adapter = adapter
         }
 
@@ -57,7 +64,45 @@ class TableFragment : Fragment() {
     }
 
     public fun updateContent() {
-        adapter = ArrayAdapter(activity, android.R.layout.simple_list_item_1, Tables[tableIndex].dishes())
+        adapter = MyListAdapter()
         list?.adapter = adapter
     }
+
+    private inner class MyListAdapter(): BaseAdapter() {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+            val dishRow: View
+
+            if (convertView == null) {
+                val inflater = LayoutInflater.from(parent?.context)
+                dishRow = inflater.inflate(R.layout.dish_ordered_list_row, parent, false)
+
+                val viewHolder = ViewHolder(dishRow.dish_textview, dishRow.dish_notes_textview)
+                dishRow.tag = viewHolder
+            } else{
+                dishRow = convertView
+            }
+
+            val viewHolder = dishRow.tag as ViewHolder
+
+            viewHolder.dishTextView.text = orderedDishes[position].toString()
+            viewHolder.dishNotesTextView.text = orderedDishes[position].notes
+
+            return dishRow
+        }
+
+        override fun getItem(position: Int): Any {
+            return orderedDishes[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return orderedDishes.count()
+        }
+    }
+
+    private class ViewHolder(val dishTextView: TextView, val dishNotesTextView: TextView)
 }
